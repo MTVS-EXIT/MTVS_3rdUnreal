29bcd190738ff7bd58ec1e349f66652da1be70c3
UmrealEngine <kr.suhyeok@gmail.com>
2024-08-27 10:27:11 +0900

    Player Basic Movement Setting

--- Source/MTVS_3rdUnreal/Private/JSH/JSH_GameModeBase.cpp
+++ Source/MTVS_3rdUnreal/Private/JSH/JSH_GameModeBase.cpp
@@ -3,3 +3,18 @@
 
 #include "JSH/JSH_GameModeBase.h"
 
+
+
+// #include "TP_ThirdPersonGameMode.h"
+// #include "JSH/JSH_Player.h"
+// #include "UObject/ConstructorHelpers.h"
+
+// ATP_ThirdPersonGameMode::ATP_ThirdPersonGameMode()
+// {
+// 	// set default pawn class to our Blueprinted character
+// 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+// 	if (PlayerPawnBPClass.Class != NULL)
+// 	{
+// 		DefaultPawnClass = PlayerPawnBPClass.Class;
+// 	}
+// }
--- Source/MTVS_3rdUnreal/Public/JSH/JSH_GameModeBase.h
+++ Source/MTVS_3rdUnreal/Public/JSH/JSH_GameModeBase.h
@@ -13,5 +13,20 @@ UCLASS()
 class MTVS_3RDUNREAL_API AJSH_GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
 };
+
+
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "GameFramework/GameModeBase.h"
+// #include "TP_ThirdPersonGameMode.generated.h"
+//
+// UCLASS(minimalapi)
+// class ATP_ThirdPersonGameMode : public AGameModeBase
+// {
+// 	GENERATED_BODY()
+//
+// public:
+// 	ATP_ThirdPersonGameMode();
+// };
