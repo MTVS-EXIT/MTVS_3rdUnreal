b00e642923ae4cc39b660b75514c7aa2e7740ccb
Jonghun96 <kjh11538@naver.com>
2024-08-31 17:25:47 +0900

    24.08.31 (New TestStart)

---
+++ Source/MTVS_3rdUnreal/Public/KJH/KJH_InGameWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KJH_InGameWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS_3RDUNREAL_API UKJH_InGameWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MTVS_3rdUnreal/Public/KJH/KJH_WidgetSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KJH_WidgetSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS_3RDUNREAL_API UKJH_WidgetSystem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
