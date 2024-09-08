fea74f1a144673eadc131e343de08d4e70f0411e
UmrealEngine <kr.suhyeok@gmail.com>
2024-09-08 14:22:18 +0900

    multi test

--- Source/MTVS_3rdUnreal/Private/KJH/KJH_GameInstance.cpp
+++ Source/MTVS_3rdUnreal/Private/KJH/KJH_GameInstance.cpp
@@ -66,8 +66,8 @@ void UKJH_GameInstance::OnCreateSessionComplete(FName SessionName, bool Success)
 	//// 내가 설정한 맵으로 listen 서버를 열고 이동한다.
 	//GetWorld()->ServerTravel(TEXT("/Game/MAPS/KJH/KJH_TestMap?listen"));
 
-	// 내가 설정한 맵으로 listen 서버를 열고 이동한다.
-	GetWorld()->ServerTravel(TEXT("/Game/ProtoMap/ProtoPT?listen"));
+   // 수혁이 맵으로 listen 서버를 열고 이동한다.
+	GetWorld()->ServerTravel(TEXT("/Game/Blueprints/Player/JSH_TMap?listen"));
 }
 
 
