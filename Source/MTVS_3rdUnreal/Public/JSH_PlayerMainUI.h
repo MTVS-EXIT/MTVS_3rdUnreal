d0c29b49df572c878dfa48448afe9781999563ea
UmrealEngine <kr.suhyeok@gmail.com>
2024-09-23 13:35:05 +0900

    UI C++ Update

---
+++ Source/MTVS_3rdUnreal/Public/JSH_PlayerMainUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "JSH_PlayerMainUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS_3RDUNREAL_API UJSH_PlayerMainUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
