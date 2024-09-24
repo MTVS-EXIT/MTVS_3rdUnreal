d804098cee6a296b24e9d77e24534c2bb85fed2a
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2024-09-24 14:21:48 +0900

    Pixel streaming 구현

---
+++ Source/MTVS_3rdUnreal/Private/KHS/KHS_PixelStreamer.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KHS/KHS_PixelStreamer.h"
+
+// Sets default values
+AKHS_PixelStreamer::AKHS_PixelStreamer()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKHS_PixelStreamer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKHS_PixelStreamer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
